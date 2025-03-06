7f89e9af7404293a0bc19fb2881d3498a37152b2
brew0day <massartcouvertures@gmail.com>
2025-03-06 08:12:33 +0100

    update

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-// index.js
-
 const express = require('express');
 const app = express();
-
-// Render te fournit une variable d'environnement PORT
-// ou 3000 par défaut en local
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node on Render!');
+  res.send('Hello from Render!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
